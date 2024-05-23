dabe3eec5f3463ea80875fbb96436766c7297c27
Tharusha Dilhara <tharushadilhara17@gmail.com>
2024-05-23 06:15:28 +0530

    QR Code generator vers 1.0
    Completed vers 1.0 of  console based qr code generator using npm

---
+++ index.js
@@ -0,0 +1,25 @@
+import inquirer from 'inquirer';
+import qr, { image } from 'qr-image';
+import fs from 'fs';
+
+inquirer
+  .prompt([
+    {
+        'message':'Type your URL:',
+        'name':'URL',
+    }
+  ])
+  .then((answers) => {
+    var url = answers.URL;
+    var qr_svg = qr.image(url);
+    var imageName = url.replaceAll('.','_');
+    qr_svg.pipe(fs.createWriteStream(imageName+'.png'));
+
+    fs.writeFile(imageName+".txt",url,(error) => {
+        if (error) throw error;
+        console.log('The file has been saved!');
+    })
+  })
+  .catch((error) => {
+    console.log(error);
+  });
